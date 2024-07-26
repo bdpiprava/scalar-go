e9991efae2316a395449a0bb99bc92382dfb87da
Bhupendrakumar Piprava <bhupendrakumar.piprava@hellofresh.com>
2024-07-26 05:48:03 +0200

    Update component with missing keys

--- model/component.go
+++ model/component.go
@@ -2,6 +2,14 @@ package model
 
 // Components holds a set of reusable objects for different aspects of the OAS.
 type Components struct {
-	Schemas    GenericObject `yaml:"schemas" json:"schemas"`
-	Parameters GenericObject `yaml:"parameters" json:"parameters"`
+	Schemas         GenericObject `yaml:"schemas" json:"schemas"`
+	Parameters      GenericObject `yaml:"parameters" json:"parameters"`
+	Responses       GenericObject `yaml:"responses" json:"responses"`
+	Examples        GenericObject `yaml:"examples" json:"examples"`
+	RequestBodies   GenericObject `yaml:"requestBodies" json:"requestBodies"`
+	Headers         GenericObject `yaml:"headers" json:"headers"`
+	SecuritySchemes GenericObject `yaml:"securitySchemes" json:"securitySchemes"`
+	Link            GenericObject `yaml:"links" json:"links"`
+	Callbacks       GenericObject `yaml:"callbacks" json:"callbacks"`
+	PathItems       GenericObject `yaml:"pathItems" json:"pathItems"`
 }
